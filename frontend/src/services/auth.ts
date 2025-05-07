16caf29284c780e034412106d3a9d5007382c6a5
Gabriel <eder.silva@montreal.com.br>
2025-05-07 11:32:53 -0300

    feat: update import paths in auth service to use alias and streamline service functions

--- frontend/src/services/auth.ts
+++ frontend/src/services/auth.ts
@@ -1,16 +1,18 @@
-import { api } from "../utils"
+import { api } from "@/utils"
 
 import {
     ILoginBody,
     ILoginResponse,
+    IMeResponse,
     IRegisterBody,
     IRegisterResponse,
-} from "../@types";
+} from "@/@types";
 
-export const loginService = async (loginBody: ILoginBody) => {
-    return api<ILoginResponse>('login').post<ILoginBody>(loginBody);
-}
+export const loginService = async (loginBody: ILoginBody) =>
+    api<ILoginResponse>('login').post<ILoginBody>(loginBody);
 
-export const registerService = async (registerBody: IRegisterBody) => {
-    return api<IRegisterResponse>('login').post<IRegisterBody>(registerBody);
-}
+export const registerService = async (registerBody: IRegisterBody) =>
+    api<IRegisterResponse>('login').post<IRegisterBody>(registerBody);
+
+export const getAuthenticatedUserService = async () =>
+    api<IMeResponse>('me').get();
