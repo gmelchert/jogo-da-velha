e66dd186e9d732ffeba9f97524acb055f522f483
Gabriel <eder.silva@montreal.com.br>
2025-05-08 15:17:36 -0300

    fix: correct user ID type in auth store to ensure consistency

--- frontend/src/stores/auth-store.tsx
+++ frontend/src/stores/auth-store.tsx
@@ -10,11 +10,11 @@ interface AuthStoreI {
 }
 
 export const useAuth = create<AuthStoreI>(set => ({
-    user: { username: "", id: "" ,token: "" },
+    user: { username: "", id: 0 ,token: "" },
     logged: false,
     login: (user: IUser) => set(() => ({ user, logged: true })),
     logout: () => {
         localStorage.removeItem("token");
-        set(() => ({ user: { username: "", id: "", token: "" }, logged: false }))
+        set(() => ({ user: { username: "", id: 0, token: "" }, logged: false }))
     },
 }))
